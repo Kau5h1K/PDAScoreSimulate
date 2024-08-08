21f14785ff934297142dbc135f791a4ca100d1e1
Kaushik Nishtala <mail2kaun@gmail.com>
2024-08-08 19:54:13 +0000

    Score Impact Tab Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,17 @@ import pandas as pd
 import os
 import re
 import subprocess
+import time
+import random
 
 
 def format_floats(x):
     return f'{x:.1f}' if isinstance(x, (float, int)) else x
+
+def generate_sorted_random_integers():
+    random_integers = random.sample(range(101), 4)
+    random_integers.sort()
+    return random_integers
 
 def git_pull(subfolder):
     try:
@@ -59,13 +66,12 @@ with st.sidebar:
     seed = st.number_input("Input the pseudo-random seed to reproduce the results", value=1, step=1, format="%d")
     num_simulations = st.slider("Select number of simulations: ", value=1000, min_value=1, max_value=10000, step=1)
 
+random_integers = generate_sorted_random_integers()
+
 st.title(":100: PDA Score Simulator")
 st.write("Utility tool to assess the impact of score changes when performing anomalous data cleanup in ElevanceHealth's Provider Directory data (SPS).")
 
 st.divider()
-
-markets = ['AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'FL', 'GA', 'IA', 'IN', 'KY', 'LA', 'MD', 'ME', 'MO', 'NH', 'NJ', 'NV', 'NY', 'NYWEST', 'OH', 'TN', 'TX', 'VA', 'WA', 'WI', 'WV']
-market_selected = st.selectbox(label='Choose a market', options=markets)
 
 folder_path = "data"
 filename_pattern = re.compile(r"ELIXIR_adhoc_PDAScoreAnalysis_(\d{8}).csv")
@@ -81,65 +87,83 @@ else:
     st.stop()
 
 master_df = pd.read_csv(f"./data/ELIXIR_adhoc_PDAScoreAnalysis_{timestamp}.csv")
-st.caption(f"Market Selected: {market_selected}")
-st.caption(f"ProviderFinder Extract Timestamp: {timestamp}")
 
-st.divider()
+markets = ['AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'FL', 'GA', 'IA', 'IN', 'KY', 'LA', 'MD', 'ME', 'MO', 'NH', 'NJ', 'NV', 'NY', 'NYWEST', 'OH', 'TN', 'TX', 'VA', 'WA', 'WI', 'WV']
+market_selected = st.selectbox(label='Choose a market', options=markets)
 
 filtered_df = master_df[master_df['MARKET'] == market_selected]
 
-if filtered_df.empty:
-    st.error(f"No matching value found for {market_selected} in data")
-    st.stop()
+st.caption(f"Market Selected: {market_selected}")
+st.caption(f"ProviderFinder Extract Timestamp: {filtered_df['TIMESTAMP'].iloc[0]}")
 
-data = {
-    'NUM_RECORDS': [filtered_df['NUM_RECORDS'].iloc[0]],
-    'ADDRESS_FINAL_AUTO': [filtered_df['ADDRESS_FINAL_AUTO'].iloc[0]],
-    'ADDRESS_FINAL_MANUAL': [filtered_df['ADDRESS_FINAL_MANUAL'].iloc[0]],
-    'ADDRESS_FINAL_GOOD': [filtered_df['ADDRESS_FINAL_GOOD'].iloc[0]],
-    'PHONE_FINAL_AUTO': [filtered_df['PHONE_FINAL_AUTO'].iloc[0]],
-    'PHONE_FINAL_MANUAL': [filtered_df['PHONE_FINAL_MANUAL'].iloc[0]],
-    'PHONE_FINAL_GOOD': [filtered_df['PHONE_FINAL_GOOD'].iloc[0]],
-    'SPCLTY_AUTO': [filtered_df['SPCLTY_AUTO'].iloc[0]],
-    'SPCLTY_MANUAL': [filtered_df['SPCLTY_MANUAL'].iloc[0]],
-    'SPCLTY_GOOD': [filtered_df['SPCLTY_GOOD'].iloc[0]]
-}
+with st.sidebar:
+    st.caption(f"Last Refresh Timestamp: {timestamp}")
 
-result_df = pd.DataFrame(data).transpose()
-result_df.reset_index(inplace=True)
-result_df.columns = ['Statistic', 'Volume']
+tab1, tab2 = st.tabs(["Score Prediction", "Manual Filter Impact"])
 
-if 'reco_bd' not in st.session_state:
+with tab1:
+    if filtered_df.empty:
+        st.error(f"No matching value found for {market_selected} in data")
+        st.stop()
+
+    data = {
+        'NUM_RECORDS': [filtered_df['NUM_RECORDS'].iloc[0]],
+        'ADDRESS_FINAL_AUTO': [filtered_df['ADDRESS_FINAL_AUTO'].iloc[0]],
+        'ADDRESS_FINAL_MANUAL': [filtered_df['ADDRESS_FINAL_MANUAL'].iloc[0]],
+        'ADDRESS_FINAL_GOOD': [filtered_df['ADDRESS_FINAL_GOOD'].iloc[0]],
+        'PHONE_FINAL_AUTO': [filtered_df['PHONE_FINAL_AUTO'].iloc[0]],
+        'PHONE_FINAL_MANUAL': [filtered_df['PHONE_FINAL_MANUAL'].iloc[0]],
+        'PHONE_FINAL_GOOD': [filtered_df['PHONE_FINAL_GOOD'].iloc[0]],
+        'SPCLTY_AUTO': [filtered_df['SPCLTY_AUTO'].iloc[0]],
+        'SPCLTY_MANUAL': [filtered_df['SPCLTY_MANUAL'].iloc[0]],
+        'SPCLTY_GOOD': [filtered_df['SPCLTY_GOOD'].iloc[0]]
+    }
+
+    result_df = pd.DataFrame(data).transpose()
+    result_df.reset_index(inplace=True)
+    result_df.columns = ['Statistic', 'Volume']
+
+    st.subheader("Data Quality Recommendations Breakdown:")
+    st.info("Edit the values to tune the predictions")
+
+    edited_df = st.data_editor(result_df, key='reco', hide_index=True)
     st.session_state['reco_bd'] = result_df
+        
+    if st.button('Apply Changes'):
+        st.divider()
+        st.subheader("Estimated Demographic Scores:")
+        bar = st.progress(random_integers[0])
+        time.sleep(1)
+        st.session_state['reco_bd'] = edited_df
 
-st.subheader("Data Quality Recommendations Breakdown:")
-st.caption("Edit the values to tune the predictions:")
+        edited_df = st.session_state['reco_bd']
+        num_records = int(edited_df.loc[edited_df['Statistic'] == 'NUM_RECORDS', 'Volume'].values[0])
+        address_auto = float(edited_df.loc[edited_df['Statistic'] == 'ADDRESS_FINAL_AUTO', 'Volume'].values[0])
+        address_manual = float(edited_df.loc[edited_df['Statistic'] == 'ADDRESS_FINAL_MANUAL', 'Volume'].values[0])
+        address_good = float(edited_df.loc[edited_df['Statistic'] == 'ADDRESS_FINAL_GOOD', 'Volume'].values[0])
+        phone_auto = float(edited_df.loc[edited_df['Statistic'] == 'PHONE_FINAL_AUTO', 'Volume'].values[0])
+        phone_manual = float(edited_df.loc[edited_df['Statistic'] == 'PHONE_FINAL_MANUAL', 'Volume'].values[0])
+        phone_good = float(edited_df.loc[edited_df['Statistic'] == 'PHONE_FINAL_GOOD', 'Volume'].values[0])
+        specialty_auto = float(edited_df.loc[edited_df['Statistic'] == 'SPCLTY_AUTO', 'Volume'].values[0])
+        specialty_manual = float(edited_df.loc[edited_df['Statistic'] == 'SPCLTY_MANUAL', 'Volume'].values[0])
+        specialty_good = float(edited_df.loc[edited_df['Statistic'] == 'SPCLTY_GOOD', 'Volume'].values[0])
 
-edited_df = st.data_editor(st.session_state['reco_bd'], key='reco', hide_index=True)
+        bar.progress(random_integers[1])
+        time.sleep(1)
 
-if st.button('Apply Changes'):
-    st.session_state['reco_bd'] = edited_df
+        average_scores_df = run_score_experiment(seed, num_simulations, num_records, address_auto, address_manual, address_good, phone_auto, phone_manual, phone_good, specialty_auto, specialty_manual, specialty_good)
+        st.session_state['average_scores_df'] = average_scores_df
 
-    num_records = int(edited_df.loc[edited_df['Statistic'] == 'NUM_RECORDS', 'Volume'].values[0])
-    address_auto = float(edited_df.loc[edited_df['Statistic'] == 'ADDRESS_FINAL_AUTO', 'Volume'].values[0])
-    address_manual = float(edited_df.loc[edited_df['Statistic'] == 'ADDRESS_FINAL_MANUAL', 'Volume'].values[0])
-    address_good = float(edited_df.loc[edited_df['Statistic'] == 'ADDRESS_FINAL_GOOD', 'Volume'].values[0])
-    phone_auto = float(edited_df.loc[edited_df['Statistic'] == 'PHONE_FINAL_AUTO', 'Volume'].values[0])
-    phone_manual = float(edited_df.loc[edited_df['Statistic'] == 'PHONE_FINAL_MANUAL', 'Volume'].values[0])
-    phone_good = float(edited_df.loc[edited_df['Statistic'] == 'PHONE_FINAL_GOOD', 'Volume'].values[0])
-    specialty_auto = float(edited_df.loc[edited_df['Statistic'] == 'SPCLTY_AUTO', 'Volume'].values[0])
-    specialty_manual = float(edited_df.loc[edited_df['Statistic'] == 'SPCLTY_MANUAL', 'Volume'].values[0])
-    specialty_good = float(edited_df.loc[edited_df['Statistic'] == 'SPCLTY_GOOD', 'Volume'].values[0])
-
-    average_scores_df = run_score_experiment(seed, num_simulations, num_records, address_auto, address_manual, address_good, phone_auto, phone_manual, phone_good, specialty_auto, specialty_manual, specialty_good)
-    st.session_state['average_scores_df'] = average_scores_df
-
-st.divider()
+        bar.progress(random_integers[2])
+        time.sleep(1)
 
 
-if 'average_scores_df' in st.session_state:
-    st.subheader("Estimated Demographic Scores:")
-    data = st.session_state['average_scores_df'].round(1).to_dict(orient='records')
-    # Apply the formatting function to the DataFrame
-    df_formatted = st.session_state['average_scores_df'].applymap(format_floats)
-    st.table(df_formatted)
+        if 'average_scores_df' in st.session_state:
+            data = st.session_state['average_scores_df'].round(1).to_dict(orient='records')
+            bar.progress(random_integers[3])
+            time.sleep(1)
+            bar.progress(100)
+            df_formatted = st.session_state['average_scores_df'].map(format_floats)
+            st.dataframe(df_formatted, hide_index=True)
+            st.warning('Tune the above attribute-wise scores to sync with Elixir-P UI for more accurate predictions')
+            st.toast('Process Finished! Please review the results.')
